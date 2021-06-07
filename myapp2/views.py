423f7029c00aa6d7c48b5bbc2502408d2936580e
Your Name <you@example.com>
2021-06-07 16:23:29 +0200

    fs

--- myapp2/views.py
+++ myapp2/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import *
+import random
+
+def index(request):
+    if "user_id" in request.session :
+        return redirect("/order/add")
+    return render(request, "index.html")
